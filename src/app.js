b27ab5c97893ffa6f41ff6fdbdfb7e7a13875af2
Nicolas <nico.06.89crc@gmail.com>
2022-04-30 01:55:05 -0300

    sss

--- src/app.js
+++ src/app.js
@@ -2,23 +2,15 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const {
-  productsRoutes,
-  brandsRoutes,
-  usersRoutes,
-  seedDbRoutes,
-} = require("./routes");
+const { productsRoutes, brandsRoutes, usersRoutes, seedDbRoutes } = require("./routes");
 const { unknownEndpoint, globalErrorHandler } = require("../src/middlewares");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(
-  "/uploads/images/brands",
-  express.static(path.join("uploads", "images", "brands"))
-);
-
+app.use("/uploads/images/brands", express.static(path.join("uploads", "images", "brands")));
+//ssss
 app.use("/api/products", productsRoutes);
 app.use("/api/brands", brandsRoutes);
 app.use("/api/users", usersRoutes);
